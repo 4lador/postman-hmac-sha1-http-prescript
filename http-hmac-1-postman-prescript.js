fdd6830e281cb4ce4c0336fa1400c10554276644
4lador <4lador@gmail.com>
2018-10-22 23:04:33 +0200

    Permits to define multiple headers at once into a Map object and dynamically adds the headers labels into the authorization string

--- http-hmac-1-postman-prescript.js
+++ http-hmac-1-postman-prescript.js
@@ -1,19 +1,3 @@
-/**
- * @Author: 4lador
- * date: 10/22/2018
- * description: Postman Pre-Request Script that Generate HMAC-SHA1 valid 'Authorization' header following HTTP signature scheme.
- *              details: https://github.com/arkerone/api-key-auth/blob/HEAD/signature.md
- *              ex: 'Signature keyid="123456789",algorithm="hmac-sha1",headers="date",signature="X+DG+jRFd/4C+81XmUXQoimry7c="'
- *              Useful for app authentication combined with this package: https://github.com/arkerone/api-key-auth#readme
- */ 
-
-// definitions
-function encode(message, key) {
-    console.log('generateSignature("' + message + '", ' + key + ');');
-    var enc = CryptoJS.HmacSHA1(message, key).toString(CryptoJS.enc.Base64);
-    return enc;
-}
-
 // Set Date
 pm.environment.set("date", new Date().toUTCString());
 var date = pm.environment.get("date");
@@ -22,14 +6,38 @@ var date = pm.environment.get("date");
 var publicKey = pm.environment.get("public_key");
 var privateKey = pm.environment.get("private_key");
 
-// Get all needed strings
-var message = 'date: ' + date;
-var signature = encode(message, privateKey);
+// Set your headers here
+var headersMap = new Map();
+headersMap.set('date', date);
+headersMap.set('content-type', 'application/json');
 
-// Generate authorization string
-var authorization = 'Signature keyid="' + publicKey + '",algorithm="hmac-sha1",headers="date",signature="' + signature + '"';
+// iterate over the headers map and create the header string
+var headersArr = [];
+var headersNames = [];
+var headersMap = headersMap;
+headersMap.forEach(function(value, key) {
+    headersArr.push(key + ': ' + value);
+    headersNames.push(key);
+});
+var sHeaders = headersArr.join('\n');
+var headersLabel = headersNames.join(' ');
 
+// sign the header string
+var signature = generateSignature(sHeaders, privateKey);
+
+// generate authorization string
+var authorization = 'Signature keyid="' + publicKey + '",algorithm="hmac-sha1",headers="' + headersLabel + '",signature="' + signature + '"';
+
+// log results for debugging
 console.log('signature: ' + signature);
 console.log('authorization: ' + authorization);
 
+// finally, set the authorization signature
 pm.environment.set("authString", authorization);
+
+// functions
+function generateSignature(message, key) {
+    console.log('generateSignature("' + message + '", ' + key + ');');
+    var enc = CryptoJS.HmacSHA1(message, key).toString(CryptoJS.enc.Base64);
+    return enc;
+}
